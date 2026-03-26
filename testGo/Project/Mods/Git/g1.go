bb02609d80c1f534fb81604427c5175f0d10cbd9
JayaHarrishM <harrishcit@gmail.com>
2026-03-26 14:35:48 +0530

    updated git go files

--- testGo/Project/Mods/Git/g1.go
+++ testGo/Project/Mods/Git/g1.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+const ver = "2.0.0"
+
 func PrintGit1(a string) {
-	fmt.Println("Git1: ", a)
+	fmt.Println("Git1 ver-", ver, " : ", a)
+
 }
--- testGo/Project/Mods/Git/g2.go
+++ testGo/Project/Mods/Git/g2.go
@@ -5,5 +5,5 @@ import (
 )
 
 func PrintGit2(a string) {
-	fmt.Println("Git2: ", a)
+	fmt.Println("Git2 ver-", ver, " : ", a)
 }
